c6372bb0dc2c893c4f66b3b61c101936f2ecf35a
Bindukksb <bindukksb@gmail.com>
2016-07-06 12:00:34 +0530

    Create possitive.java

---
+++ possitive.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class possitive
+{
+public static void main(String args[])
+{
+int n;
+Scanner s=new Scanner(System.in);
+System.out.println("enter the value");
+n=s.nextInt();
+if(n>0)
+{
+System.out.println("given value is possitive");
+}
+else
+{
+System.out.println("given value is negative");
+}
+}
+}
